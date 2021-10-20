d1336b5a0954c4c526ec820cbec33ba7036f36ca
smee6 <smee6@naver.com>
2021-10-20 16:15:53 +0900

    WEB

--- src/main/java/jpastudy/jpashop/repository/OrderRepository.java
+++ src/main/java/jpastudy/jpashop/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package jpastudy.jpashop.repository;
 
-import jpastudy.jpashop.domain.Order;
-import jpastudy.jpashop.domain.OrderSearch;
-import org.aspectj.weaver.ast.Or;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpastudy.jpashop.domain.*;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -23,6 +25,37 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
-        return em.createQuery("select o from Order o",Order.class).getResultList();
+        //return em.createQuery("select o from Order o",Order.class).getResultList();
+        //1. JPA query Factory 생성
+        JPAQueryFactory query = new JPAQueryFactory(em);
+
+        //2. Qorder, Qmember를 가지고 온다.
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        //return
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member,member)
+                .where(statusEq(orderSearch.getOrderStatus()),
+                       nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if(!StringUtils.hasText(memberName)){
+            return null;
+        }
+        return QMember.member.name.contains(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus orderStatus) {
+        if(orderStatus == null){
+            return null;
+        }
+        return QOrder.order.status.eq(orderStatus);
     }
 }
---
+++ src/main/java/jpastudy/jpashop/web/HomeController.java
@@ -0,0 +1,24 @@
+package jpastudy.jpashop.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+    @RequestMapping("/fragments")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
+
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
+
+    }
+
+}
