905f68850ace0d973d3d4f688c39d488eb80d0fd
smee6 <smee6@naver.com>
2021-10-20 10:34:30 +0900

    Order order item , biz logic

--- src/main/java/jpastudy/jpashop/domain/Order.java
+++ src/main/java/jpastudy/jpashop/domain/Order.java
@@ -13,12 +13,13 @@ import java.util.List;
 
 @Entity
 @Table(name = "orders")
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column (name = "order_id")
+    @Column(name = "order_id")
     private Long id;
 
     //Member와 N:1 관계
@@ -33,8 +34,8 @@ public class Order {
 
     //OrderItem 과의 관계 1:N   <= cf) n이 주인 : 외래키 가짐 : 그쪽에다가 조인 어쩌고 선언
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // 1:N에서 1쪽은 캐스케이드 해줘야 한다 => 그래야 같이 움직이지
-    private List<OrderItem> orderItems =new ArrayList<>();
-    
+    private List<OrderItem> orderItems = new ArrayList<>();
+
     //주문 날짜와 시간
     private LocalDateTime orderDate;
 
@@ -47,15 +48,57 @@ public class Order {
         this.member = member;
         member.getOrders().add(this);
     }
+
     // 1:1관계
     public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
     // 1:N 관계
-    public void addOrderItem(OrderItem orderItem){
+    public void addOrderItem(OrderItem orderItem) {
         this.orderItems.add(orderItem);
         orderItem.setOrder(this);
+    }
+
+
+    //== 비즈니스 로직 : 주문생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+
+        order.setMember(member);
+        // 주문한 회원 연결
+        order.setDelivery(delivery);
+        // 배송과 오더 연결
+
+        //Order와 Order Item 을 연결
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        //주문상태 변경경
+       order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직 : 주문 취소 ==//
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==비즈니스 로직 : 전체 주문 가격 조회 ==//
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 
 }
--- src/main/java/jpastudy/jpashop/domain/OrderItem.java
+++ src/main/java/jpastudy/jpashop/domain/OrderItem.java
@@ -10,17 +10,19 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "order_item")
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="order_item_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id")
     private Long id;
 
     //Order와의 관계 N:1 ... 지금 아이템이 N이지? N이 주인이지 그럼 외래키 가져야지? 예~~
     //조인컬럼 있어야겠지 then~
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="order_id")
+    @JoinColumn(name = "order_id")
     private Order order;
 
     //Item 과의 관계는 N:1이다
@@ -31,5 +33,29 @@ public class OrderItem {
     //가격과 수량
     private int orderPrice;
     private int count;
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+
+        //OrderItem과 Item 연결
+        orderItem.setItem(item);
+        //주문 상품 가격 저장
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비즈니스 로직 : 주문 취소 ==//
+    public void cancel() {
+        getItem().addStock(this.count);
+    }
+
+    //==비즈니스 로직 : 주문상품 전체 가격 조회 ==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
 
 }
